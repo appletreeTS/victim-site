2269ca514b0ad15cd9611b02f92a7bc57e1aab77
appletreeTS <150872121+appletreeTS@users.noreply.github.com>
2026-04-22 16:24:49 +0900

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,10 @@
-
+<?php
+$db_host = '127.0.0.1';
+$db_user = 'victim';
+$db_pass = 'victim';
+$db_name = 'victim_db';
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+if (!$conn) {
+    die('DB connect failed: ' . mysqli_connect_error());
+}
+?>
